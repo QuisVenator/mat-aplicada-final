5ba71e165b6a4396da005d817e16304123c24723
Manuel Rene Pauls Toews <contact@renepauls.com>
2022-10-28 22:59:48 -0300

    COMPILA

--- regla.py
+++ regla.py
@@ -1,3 +1,6 @@
+from shapely.geometry import Polygon, MultiPolygon
+from shapely.ops import unary_union
+
 class Regla:
     def __init__(self, humor, f_externos, v_precio, salida):
         self.humor = humor
@@ -5,58 +8,68 @@ class Regla:
         self.v_precio = v_precio
         self.salida = salida
     
-    def disparo(self, pertenencias):
-        return (self.salida, min(pertenencias[self.humor], pertenencias[self.f_externos], pertenencias[self.v_precio]))
+    def evaluar(self, pertenencias):
+        val = min(pertenencias["humor"][self.humor], pertenencias["f_externos"][self.f_externos], pertenencias["v_precio"][self.v_precio])
+        return (self.salida, val if val > 0 else None)
 
 class FuzzySet:
-    def __init__(self, nombre, min, c1, c2, max):
-        self.nombre = nombre
+    def __init__(self, min, c1, c2, max, val_extremo_neg=0, val_extremo_pos=0):
         self.min = min
         self.c1 = c1
         self.c2 = c2
         self.max = max
+        self.val_extremo_neg = val_extremo_neg
+        self.val_extremo_pos = val_extremo_pos
     
     def pertenencia(self, x):
-        if x <= self.min or x >= self.max:
-            return 0
+        if x <= self.min:
+            return self.val_extremo_neg
+        elif x >= self.max:
+            return self.val_extremo_pos
         elif x >= self.c1 and x <= self.c2:
             return 1
         elif x > self.min and x < self.c1:
             return (x - self.min) / (self.c1 - self.min)
         elif x > self.c2 and x < self.max:
             return (x - self.c2) / (self.max - self.c2) * -1 + 1
+    
+    def poligono(self, y):
+        puntos = [(self.min if self.min != float('-inf') else self.start, 0)]
+        puntos.append((y * (self.c1 - self.min) + self.min, y))
+        puntos.append((y * (self.max - self.c2) + self.c2, y))
+        puntos.append((self.max, 0) if self.min != float('inf') else self.stop)
 
-# main function
+        return Polygon(puntos)
+
 def main():
     sets = {
-        "humor": [
-            FuzzySet("panico", float('-inf'), float('-inf'), -0.8, -0.2),
-            FuzzySet("ansiedad", -0.8, -0.2, 0, 0.01),
-            FuzzySet("alegria", -0.01, 0, 0.2, 0.8),
-            FuzzySet("euforia", 0.3, 0.8, float('inf'), float('inf'))
-        ],
-        "f_externos": [
-            FuzzySet("muy_negativo", float('-inf'), float('-inf'), -0.8, -0.2),
-            FuzzySet("poco_negativo", -0.8, -0.2, 0, 0.01),
-            FuzzySet("poco_positivo", -0.01, 0, 0.2, 0.8),
-            FuzzySet("muy_positivo", 0.3, 0.8, float('inf'), float('inf'))
-        ],
-        "v_precio": [
-            FuzzySet("muy_negativo", float('-inf'), float('-inf'), -9, -1),
-            FuzzySet("poco_negativo", -9, -2, 0, 0.01),
-            FuzzySet("poco_positivo", 0.01, 0, 2, 9),
-            FuzzySet("muy_positivo", 1, 9, float('inf'), float('inf'))
-        ],
-        "salida": [
-            FuzzySet("pesimista", float('-inf'), float('-inf'), -0.8, -0.2),
-            FuzzySet("poco_pesimista", -0.8, -0.2, 0, 0.01),
-            FuzzySet("poco_optimista", -0.01, 0, 0.2, 0.8),
-            FuzzySet("optimista", 0.3, 0.8, float('inf'), float('inf'))
-        ]
+        "humor": {
+            "panico": FuzzySet(-1, -1, -0.8, -0.2, val_extremo_neg=1),
+            "ansiedad": FuzzySet(-0.8, -0.2, 0, 0.01),
+            "alegria": FuzzySet(-0.01, 0, 0.2, 0.8),
+            "euforia": FuzzySet(0.3, 0.8, 1, 1, val_extremo_pos=1)
+        },
+        "f_externos": {
+            "muy_negativo": FuzzySet(-1, -1, -0.8, -0.2, val_extremo_neg=1),
+            "poco_negativo": FuzzySet(-0.8, -0.2, 0, 0.01),
+            "poco_positivo": FuzzySet(-0.01, 0, 0.2, 0.8),
+            "muy_positivo": FuzzySet(0.3, 0.8, 1, 1, val_extremo_pos=1)
+        },
+        "v_precio": {
+            "muy_negativo": FuzzySet(-12, -12, -9, -1, val_extremo_neg=1),
+            "poco_negativo": FuzzySet(-9, -2, 0, 0.01),
+            "poco_positivo": FuzzySet(0.01, 0, 2, 9),
+            "muy_positivo": FuzzySet(1, 9, 12, 12, val_extremo_pos=1)
+        },
+        "salida": {
+            "pesimista": FuzzySet(-1, -1, -0.8, -0.2, val_extremo_neg=1),
+            "poco_pesimista": FuzzySet(-0.8, -0.2, 0, 0.01),
+            "poco_optimista": FuzzySet(-0.01, 0, 0.2, 0.8),
+            "optimista": FuzzySet(0.3, 0.8, 1, 1, val_extremo_pos=1)
+        }
     }
 
     reglas = [
-        # Regla(humor, f_externos, v_precio, salida)
         Regla("panico", "muy_negativo", "muy_negativo", "pesimista"),
         Regla("panico", "muy_negativo", "poco_negativo", "pesimista"),
         Regla("panico", "muy_negativo", "poco_positivo", "poco_pesimista"),
@@ -126,6 +139,34 @@ def main():
     v_precio = float(input("V. precio: "))
     humor = float(input("Humor: "))
     f_externos = float(input("F. externos: "))
+
+    pertenencias = {
+        "humor": {
+            "panico": sets["humor"]["panico"].pertenencia(humor),
+            "ansiedad": sets["humor"]["ansiedad"].pertenencia(humor),
+            "alegria": sets["humor"]["alegria"].pertenencia(humor),
+            "euforia": sets["humor"]["euforia"].pertenencia(humor),
+        },
+        "f_externos": {
+            "muy_negativo": sets["f_externos"]["muy_negativo"].pertenencia(f_externos),
+            "poco_negativo": sets["f_externos"]["poco_negativo"].pertenencia(f_externos),
+            "poco_positivo": sets["f_externos"]["poco_positivo"].pertenencia(f_externos),
+            "muy_positivo": sets["f_externos"]["muy_positivo"].pertenencia(f_externos),
+        },
+        "v_precio": {
+            "muy_negativo": sets["v_precio"]["muy_negativo"].pertenencia(v_precio),
+            "poco_negativo": sets["v_precio"]["poco_negativo"].pertenencia(v_precio),
+            "poco_positivo": sets["v_precio"]["poco_positivo"].pertenencia(v_precio),
+            "muy_positivo": sets["v_precio"]["muy_positivo"].pertenencia(v_precio),
+        }
+    }
+
+    res = [regla.evaluar(pertenencias) for regla in reglas]
+    res_poly = [sets["salida"][r[0]].poligono(r[1]) for r in res if r[1]]
+    fuzzy_result = unary_union(res_poly)
+    print(fuzzy_result)
+    scalar_result = fuzzy_result.centroid.x
+    print("Resultado: ", scalar_result)
 
 if __name__ == "__main__":
     main()
